c88a3d394c49a364787b4b376363021c5d05476e
blascarr <adrian.blascarr@gmail.com>
2024-01-20 17:27:52 +0100

    Layout definition

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,22 +6,19 @@ const PORT = Number(process.env.PORT) || 3000;
 
 const app = new Elysia()
 	.use(staticPlugin())
-	.get('/', () => Bun.file('public/index.html'))
+	.get('/', ({ params: { id } }) => {
+		return { id: id, title: 'Learn Buns' };
+	})
 	.get('/dashboard', ({ params: { id } }) => {
 		return { id: id, title: 'Learn Buns' };
 	})
-	//.get('*', () => Bun.file('dist/index.html'))
+	.get('/editor', ({ params: { id } }) => {
+		return { id: id, title: 'Learn Buns' };
+	})
 	.get('/post/:id', ({ params: { id } }) => {
 		return { id: id, title: 'Learn Buns' };
 	});
 
-/*app.group('/user', (app) =>
-	app
-		.post('/signin', () => 'Sign in route')
-		.get('/id', () => {
-			'User with ID';
-		})
-);*/
 app.listen(PORT);
 console.log(PORT);
 console.log('Listening on Port ', app.server?.port);
